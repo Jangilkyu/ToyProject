42361f3354860077e21a23c0e39aa1bc408c217e
jangilkyu <jang19.dev@gmail.com>
2022-02-22 22:40:12 +0900

    Add: gitignore

---
+++ BookSearch/BookSearch/Processor/AlamofireProcessor.swift
@@ -0,0 +1,8 @@
+//
+//  AlamofireProcessor.swift
+//  BookSearch
+//
+//  Created by 장일규 on 2022/02/22.
+//
+
+import Foundation
---
+++ BookSearch/BookSearch/Util/Storage.swift
@@ -0,0 +1,8 @@
+//
+//  Storage.swift
+//  BookSearch
+//
+//  Created by 장일규 on 2022/02/22.
+//
+
+import Foundation
